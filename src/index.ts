17e95260151ef58d0e3585e2dae912f64f582dcb
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-05-26 13:36:56 +0800

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config'
 import { OpenAI } from "langchain/llms/openai";
-
-const openAIApiKey = process.env.OPENAI_API_KEY;
-const model = new OpenAI({ openAIApiKey, temperature: 0.9 });
-console.info(model)
+const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9, stream: true});
+async function run() {
+  try {
+    const res = await model.call("怎么评价人工智能?");
+  console.log(res);
+  } catch(_err) {
+    console.error(_err)
+  }
+  
+}
+run()
